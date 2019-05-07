38131eae5d8d362982ec94bf1b1aa42d08b011af
Matias Campagna <mcampagna@inclusion.cloud>
2019-05-07 14:34:58 -0300

    creada version del script local

---
+++ README.md
@@ -0,0 +1,4 @@
+Para correrlo local, tener instalado python y pip:
+	pip install wheel
+	pip install pandas
+	pip install matplotlib
---
+++ local-python.py
@@ -0,0 +1,39 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Create dictionary
+dictionary = ['HTML','CSS','JS']
+
+# Define empty object to use later for graphic
+OBJ = {
+	'name':[],
+	'skills':[]
+}
+
+def CleanFileNames(file):
+	return file.split('.')[0].lower()
+
+# Read all source data files
+for filename in os.listdir('./data'):
+	if filename.endswith('.txt'):
+		cleanFileNames=CleanFileNames(filename)
+
+		with open(os.path.join('./data', filename), encoding='utf-8') as searchfile:
+			for line in searchfile:		# For each line of the source files
+				for term in dictionary:	# For each term from dictionary in each line
+					if term in line:
+						OBJ['name'].append(cleanFileNames)
+						OBJ['skills'].append(term)
+
+# Create dataFrame for graphic
+df=pd.DataFrame(OBJ)
+print(df)
+
+# Render graphic
+df.groupby(['name','skills']).size().unstack().plot(kind='bar',stacked=True, title='Skills')
+plt.ylabel('Count')
+plt.show()
